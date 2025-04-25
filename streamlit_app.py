1c6b9df597405ad29e4642df2712a4689b2d178d
n8jones418 <n8jones418@comcast.net>
2025-04-25 14:16:05 -0400

    Update streamlit_app.py
    updated the api call section to add together choices

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -48,9 +49,11 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success(name_on_order +', your Smoothie is ordered!', icon="✅")
 
-#New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+if ingredients_list:
+    ingredients_string=''
+
+    for fruit_chosen in ingredients_list:
+      ingredients_string += fruit_chosen + ' '
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
